e2325f87aee221ca8040e913352ae455e4c42573
Sumit <pankajsinghtomar987@gmail.com>
2025-10-16 19:38:16 +0530

    playground-file-explorer-1

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+    REACT: "/exocode-starters/react-ts",
+    NEXTJS: "/exocode-starters/nextjs-new",
+    EXPRESS: "/exocode-starters/express-simple",
+    VUE: "/exocode-starters/vue",
+    HONO: "/exocode-starters/hono-nodejs-starter",
+    ANGULAR: "/exocode-starters/angular",
+}
